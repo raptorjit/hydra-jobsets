c13fc9af35701bf35f7ebb01ac9285fa9d9e9a08
Luke Gorrie <luke@snabb.co>
2017-03-28 20:44:52 +0000

    Revert "Remove spurious (?) emailresponsible fields"
    This reverts commit 42a140815084423873ee95d46ea72e9d6474d090.

--- default.nix
+++ default.nix
@@ -17,18 +17,18 @@
         "inputs": {
           "benchmarkRunsNix": { "type": "expression", "value": 300 },
           "hardware":     { "type": "string", "value": "murren" },
-          "nixpkgs":      { "type": "git", "value": "https://github.com/NixOS/nixpkgs-channels.git nixos-16.03-small" },
+          "nixpkgs":      { "type": "git", "value": "https://github.com/NixOS/nixpkgs-channels.git nixos-16.03-small", "emailresponsible": false },
           "Aname":        { "type": "string", "value": "hot64" },
-          "Asrc":         { "type": "git", "value": "https://github.com/lukego/raptorjit proto-entropy" },
+          "Asrc":         { "type": "git", "value": "https://github.com/lukego/raptorjit proto-entropy", "emailresponsible": false },
           "Bname":        { "type": "string", "value": "hot8" },
-          "Bsrc":         { "type": "git", "value": "https://github.com/lukego/raptorjit hot8" },
+          "Bsrc":         { "type": "git", "value": "https://github.com/lukego/raptorjit hot8", "emailresponsible": false },
           "Cname":        { "type": "string", "value": "hot16" },
-          "Csrc":         { "type": "git", "value": "https://github.com/lukego/raptorjit hot16" },
+          "Csrc":         { "type": "git", "value": "https://github.com/lukego/raptorjit hot16", "emailresponsible": false },
           "Dname":        { "type": "string", "value": "hot256" },
-          "Dsrc":         { "type": "git", "value": "https://github.com/lukego/raptorjit hot256" },
+          "Dsrc":         { "type": "git", "value": "https://github.com/lukego/raptorjit hot256", "emailresponsible": false },
           "Ename":        { "type": "string", "value": "hot1024" },
-          "Esrc":         { "type": "git", "value": "https://github.com/lukego/raptorjit hot1024" },
-          "testsuiteSrc": { "type": "git", "value": "https://github.com/raptorjit/raptorjit-testsuite" }
+          "Esrc":         { "type": "git", "value": "https://github.com/lukego/raptorjit hot1024", "emailresponsible": false },
+          "testsuiteSrc": { "type": "git", "value": "https://github.com/raptorjit/raptorjit-testsuite", "emailresponsible": false }
         }
       }
     }
